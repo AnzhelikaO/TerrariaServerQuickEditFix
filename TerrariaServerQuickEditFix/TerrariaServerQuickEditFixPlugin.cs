100ccbc0fada6d8f6a7f6c451faed246c75ba252
Anzhelika <qwerty5867192@gmail.com>
2023-08-21 16:54:31 +0300

    net 6.0

--- TerrariaServerQuickEditFix/TerrariaServerQuickEditFixPlugin.cs
+++ TerrariaServerQuickEditFix/TerrariaServerQuickEditFixPlugin.cs
@@ -1,82 +1,83 @@
 ﻿#region Using
-using System;
 using System.Runtime.InteropServices;
 using Terraria;
 using TerrariaApi.Server;
 #endregion
-namespace TerrariaServerQuickEditFix
+namespace TerrariaServerQuickEditFix;
+
+[ApiVersion(2, 1)]
+public class TerrariaServerQuickEditFixPlugin : TerrariaPlugin
 {
-    [ApiVersion(2, 1)]
-    public class TerrariaServerQuickEditFixPlugin : TerrariaPlugin
+    #region Data
+
+    public override string Name => "TerrariaServer.exe QuickEdit fix";
+    public override string Author => "https://stackoverflow.com/a/36720802 applied by Anzhelika";
+    public override Version Version => new(1, 0, 0, 0);
+    public override string Description => "Enables QuickEdit mode in TerrariaServer.exe application";
+
+    public TerrariaServerQuickEditFixPlugin(Main Game)
+            : base(Game) =>
+        Order = int.MinValue;
+
+    private const uint ENABLE_QUICK_EDIT = 0x0040;
+
+    // STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
+    private const int STD_INPUT_HANDLE = -10;
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetStdHandle(int nStdHandle);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+    #endregion
+    #region Initialize, Dispose
+
+    public override void Initialize() =>
+        SwitchQuickEditMode(true);
+
+    protected override void Dispose(bool Disposing)
     {
-        #region Data
-
-        public override string Name => "TerrariaServer.exe QuickEdit fix";
-        public override string Author => "https://stackoverflow.com/a/36720802 applied by Anzhelika";
-        public override Version Version => new Version(1, 0, 0, 0);
-        public override string Description => "Enables QuickEdit mode in TerrariaServer.exe application";
-        public TerrariaServerQuickEditFixPlugin(Main Game) : base(Game) { }
-
-        const uint ENABLE_QUICK_EDIT = 0x0040;
-
-        // STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
-        const int STD_INPUT_HANDLE = -10;
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern IntPtr GetStdHandle(int nStdHandle);
-
-        [DllImport("kernel32.dll")]
-        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
-
-        [DllImport("kernel32.dll")]
-        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
-
-        #endregion
-        #region Initialize, Dispose
-
-        public override void Initialize() =>
-            SwitchQuickEditMode(true);
-
-        protected override void Dispose(bool Disposing)
-        {
-            if (Disposing)
-                SwitchQuickEditMode(false);
-            base.Dispose(Disposing);
-        }
-
-        #endregion
-        #region SwitchQuickEditMode
-
-        public static void SwitchQuickEditMode(bool Enable)
-        {
-            IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
-
-            // Get current console mode
-            if (!GetConsoleMode(consoleHandle, out uint consoleMode))
-            {
-                // ERROR: Unable to get console mode.
-                return;
-            }
-
-            if (Enable)
-            {
-                // Enable the quick edit bit in the mode flags
-                consoleMode |= ENABLE_QUICK_EDIT;
-            }
-            else
-            {
-                // Clear the quick edit bit in the mode flags
-                consoleMode &= ~ENABLE_QUICK_EDIT;
-            }
-
-            // Set the new mode
-            if (!SetConsoleMode(consoleHandle, consoleMode))
-            {
-                // ERROR: Unable to set console mode
-                return;
-            }
-        }
-
-        #endregion
+        if (Disposing)
+            SwitchQuickEditMode(false);
+        base.Dispose(Disposing);
     }
+
+    #endregion
+    #region SwitchQuickEditMode
+
+    public static void SwitchQuickEditMode(bool Enable)
+    {
+        IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+
+        // Get current console mode
+        if (!GetConsoleMode(consoleHandle, out uint consoleMode))
+        {
+            // ERROR: Unable to get console mode.
+            return;
+        }
+
+        if (Enable)
+        {
+            // Enable the quick edit bit in the mode flags
+            consoleMode |= ENABLE_QUICK_EDIT;
+        }
+        else
+        {
+            // Clear the quick edit bit in the mode flags
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+        }
+
+        // Set the new mode
+        if (!SetConsoleMode(consoleHandle, consoleMode))
+        {
+            // ERROR: Unable to set console mode
+            return;
+        }
+    }
+
+    #endregion
 }
